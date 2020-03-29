cf9e46ab61443d2410db47c52573650f092d0dae
Pedrowesley <raquel97378117@gmail.com>
2020-03-29 19:04:45 -0300

    config alterado

--- conexao/config.php
+++ conexao/config.php
@@ -1,9 +1,9 @@
 <?php
 	
-	define('DB_NAME','diario');
+	define('DB_NAME','u960663889_acompanheTeste');
 	define('DB_HOST', 'localhost');
-	define('DB_PASS','root');
-	define('DB_USER','root');
+	define('DB_PASS','residentkrauser101');
+	define('DB_USER','u960663889_pedrodev101');
 
 ?>
 
